4ed2b1dfa36cc9f7f80e344dafa130cb7032e622
alex <cyy0523xc@gmail.com>
2019-09-09 16:32:09 +0800

    start sh

--- docker-start.sh
+++ docker-start.sh
@@ -3,8 +3,8 @@
 # 
 # Author: alex
 # Created Time: 2019年09月02日 星期一 10时09分39秒
-sudo docker rm -f ibbd-openpose
-sudo docker run  -u $(id -u):$(id -g) --rm -ti --runtime=nvidia \
+docker rm -f ibbd-openpose
+docker run  -u $(id -u):$(id -g) --rm -ti --runtime=nvidia \
     -p 20950:20950 \
     --name ibbd-openpose \
     -v `pwd`:/opt/openpose/server \
