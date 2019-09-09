bc9492ba509bd24662d51c0f379199a0819c4b42
alex <cyy0523xc@gmail.com>
2019-09-09 16:28:41 +0800

    start sh

--- docker-start.sh
+++ docker-start.sh
@@ -3,12 +3,12 @@
 # 
 # Author: alex
 # Created Time: 2019年09月02日 星期一 10时09分39秒
-docker rm -f ibbd-openpose
-docker run  -u $(id -u):$(id -g) --rm -ti --runtime=nvidia \
+sudo docker rm -f ibbd-openpose
+sudo docker run  -u $(id -u):$(id -g) --rm -ti --runtime=nvidia \
     -p 20950:20950 \
     --name ibbd-openpose \
     -v `pwd`:/opt/openpose/server \
     -e PYTHONPATH=/opt/openpose/python \
     -w /opt/openpose/server \
     registry.cn-hangzhou.aliyuncs.com/ibbd/pose:openpose \
-    /bin/bash
+    $*
